4470a5dc77bd084dfb42f094fe8fda6e3c6af5da
Alandryia Jones <jonesalandryia@gmail.com>
2017-07-27 15:12:36 -0500

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import BaseLayout from "./components/BaseLayout";
+import About from "./components/About";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/home" component={Home} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
